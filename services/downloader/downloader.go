b331e52ba87ec29d2e7e813c0e03861e89703a3a
Arsham Arya <Arshamalh@gmail.com>
2023-09-06 13:08:00 +0330

    change: chunks type to buffer and progress.

--- services/downloader/downloader.go
+++ services/downloader/downloader.go
@@ -2,6 +2,7 @@ package downloader
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"sync"
+	"time"
 )
 
 // TODO: Show the downloading progress in Telegram, UI, CLI or whatever is using this application
@@ -22,13 +24,18 @@ type I interface {
 
 type downloader struct {
 	workersCount int // TODO Calculate workers count dynamically and combine its logic with process single
-	chunks       [][]byte
+	chunks       []bytes.Buffer
+	// IMPORTANT
+	// If multiple users use the same manager
+	// If each user have a manager but each user can download multiple files at the same time
+	// This logic won't work!
+	// We can also have one manager per request and that kind of make sense
 }
 
 func New(workers_count int) *downloader {
 	return &downloader{
 		workersCount: workers_count,
-		chunks:       make([][]byte, workers_count),
+		chunks:       make([]bytes.Buffer, workers_count),
 	}
 }
 
@@ -39,7 +46,12 @@ func (d *downloader) Download(uri string) (filePath string, err error) {
 		return "", err
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go d.progress(ctx, contentLength)
+
 	if !isSupported || d.workersCount <= 1 {
+		fmt.Println("processing single")
 		return d.processSingle(uri)
 	}
 
@@ -53,8 +65,7 @@ func (d *downloader) Download(uri string) (filePath string, err error) {
 }
 
 func (d *downloader) processSingle(uri string) (filePath string, err error) {
-	//Initialize first index with []byte
-	d.chunks[0] = make([]byte, 0)
+	d.chunks[0] = bytes.Buffer{}
 	d.downloadFileForRange(nil, uri, "", 0)
 
 	if err != nil {
@@ -90,7 +101,7 @@ func (d *downloader) processMultiple(contentLength int, uri string) (filePath st
 
 func (d *downloader) downloadFileForRange(wg *sync.WaitGroup, uri, _range string, index int) {
 	defer wg.Done()
-
+	fmt.Printf("\nrange %s started", _range)
 	request, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		return
@@ -100,18 +111,16 @@ func (d *downloader) downloadFileForRange(wg *sync.WaitGroup, uri, _range string
 		request.Header.Add("Range", "bytes="+_range)
 	}
 
-	sc, _, data, err := doAPICall(request)
+	response, err := (&http.Client{}).Do(request)
 	if err != nil {
-		return
+		fmt.Println(err)
 	}
+	defer response.Body.Close()
 
-	if sc != 200 && sc != 206 {
-		// err = fmt.Errorf("invalid status code: %d", sc)
-		return
-	}
-
-	d.chunks[index] = make([]byte, 0)
-	d.chunks[index] = append(d.chunks[index], data...)
+	fmt.Println("started writing to buffer")
+	d.chunks[index] = bytes.Buffer{}
+	written, err := io.Copy(&d.chunks[index], response.Body)
+	fmt.Println(written, err)
 }
 
 func (d *downloader) combineChunks(uri string) (filePath string, err error) {
@@ -127,59 +136,54 @@ func (d *downloader) combineChunks(uri string) (filePath string, err error) {
 	}
 	defer output.Close()
 
-	buf := bytes.NewBuffer(nil)
 	for i := 0; i < len(d.chunks); i++ {
-		if _, err := buf.Write(d.chunks[i]); err != nil {
+		if _, err = d.chunks[i].WriteTo(output); err != nil {
 			return "", err
 		}
-	}
-
-	if _, err = buf.WriteTo(output); err != nil {
-		return "", err
 	}
 
 	return filePath, nil
 }
 
-func getRangeDetails(uri string) (bool, int, error) {
-	request, err := http.NewRequest("HEAD", uri, nil)
-	if err != nil {
-		return false, 0, err
+func (d *downloader) progress(ctx context.Context, totalLen int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			totalDownloaded := 0
+			for _, chunk := range d.chunks {
+				totalDownloaded += int((float32(chunk.Len()) / float32(totalLen)) * 100)
+			}
+			if totalDownloaded > 100 {
+				totalDownloaded = 100
+			}
+			fmt.Println(totalDownloaded) // TODO: Don't print here, it's useless, return to a channel and write a consumer for it in other places such as UI or Telegram
+		}
+		time.Sleep(time.Millisecond * 500)
 	}
+}
 
-	sc, headers, _, err := doAPICall(request)
+func getRangeDetails(uri string) (bool, int, error) {
+	response, err := (&http.Client{}).Head(uri)
+
 	if err != nil {
 		// If resets by peer, we should tell user that we don't support downloading this podcast
 		return false, 0, err
 	}
 
-	if sc != 200 && sc != 206 {
+	if response.StatusCode != 200 && response.StatusCode != 206 {
 		return false, 0, err
 	}
 
-	contentLength, err := strconv.Atoi(headers.Get("Content-Length"))
+	contentLength, err := strconv.Atoi(response.Header.Get("Content-Length"))
 	if err != nil {
 		return false, 0, err
 	}
 
-	if headers.Get("Accept-Ranges") == "bytes" {
+	if response.Header.Get("Accept-Ranges") == "bytes" {
 		return true, contentLength, nil
 	}
 
 	return false, contentLength, nil
-}
-
-func doAPICall(request *http.Request) (statusCode int, header http.Header, data []byte, err error) {
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return 0, nil, nil, err
-	}
-	defer response.Body.Close()
-
-	data, err = io.ReadAll(response.Body)
-	if err != nil {
-		return 0, nil, nil, err
-	}
-
-	return response.StatusCode, response.Header, data, nil
 }
