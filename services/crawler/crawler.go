dd07f77b0b01480b9044658b30a3b723a23c39b0
Mobina Ezzoddin <mobina.ezzoddin@gmail.com>
2023-09-06 12:29:22 +0330

    feat: add castbox provider
    * feat: add castbox provider
    
    Find castbox podcast download link
    
    * feat: handle unknown provider

--- services/crawler/crawler.go
+++ services/crawler/crawler.go
@@ -1,6 +1,7 @@
 package crawler
 
 import (
+	"errors"
 	"github.com/gocolly/colly/v2"
 	"strings"
 )
@@ -21,13 +22,24 @@ func New() *crawler {
 // Get a page link as an input and search in the page for a podcast link,
 // returns podcast link if any or raise an error if there is no link or the page is not accessible.
 func (c crawler) Find(page_link string) (podcast_link string, err error) {
-
-	podcastLinks, err := getGooglePodcastLinks(page_link)
-	if err != nil {
-		return "", err
+	var podcastLinks []string
+	if strings.Contains(page_link, "google") {
+		podcastLinks, err = getGooglePodcastLinks(page_link)
+		if err != nil {
+			return "", err
+		}
+		return podcastLinks[0], nil
 	}
 
-	return podcastLinks[0], nil
+	if strings.Contains(page_link, "castbox") {
+		podcastLinks, err = getCastboxPodcastLinks(page_link)
+		if err != nil {
+			return "", err
+		}
+		return podcastLinks[0], nil
+	}
+
+	return "nil", errors.New("unknown provider")
 }
 
 // Find google podcast .mp3 link
@@ -40,6 +52,27 @@ func getGooglePodcastLinks(page_link string) (podcast_link []string, err error) 
 		mp3Index := strings.Index(jsdata, ".mp3")
 		if mp3Index > httpIndex {
 			podcast_link = append(podcast_link, jsdata[httpIndex:mp3Index+4])
+		}
+	})
+
+	err = cl.Visit(page_link)
+	if err != nil {
+		return nil, err
+	}
+
+	return podcast_link, nil
+}
+
+// Find castbox .mp3 link
+func getCastboxPodcastLinks(page_link string) (podcast_link []string, err error) {
+	cl := colly.NewCollector()
+
+	cl.OnHTML(`#root > div > div:nth-child(1) > audio > source `, func(e *colly.HTMLElement) {
+		link := e.Attr("src")
+		httpIndex := strings.LastIndex(link, "https")
+		mp3Index := strings.Index(link, ".mp3")
+		if mp3Index > httpIndex {
+			podcast_link = append(podcast_link, link[httpIndex:mp3Index+4])
 		}
 	})
 
